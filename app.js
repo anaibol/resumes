c2709b5f7b5117faee46e6ebdf1cf0cd96ec4ba7
anaibol <hey@anib.al>
2016-01-07 16:52:38 +0100

    Refactoring and creating the API

--- app.js
+++ app.js
@@ -4,22 +4,53 @@ var app = express();
 var jsonfile = require('jsonfile');
 
 var resumeToPDF = require('resume-to-pdf');
-var resumeToHTML = require('resume-to-html');
+var resumeToHTML = require('./resume2html');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // app.use(express.static('public'));
 
-app.use('/:name/resume.json', express.static(__dirname + '/resumes'));
-app.use('/:name/edit', express.static(__dirname + '/editor'));
+app.get('/', function (req, res) {
+  jsonfile.readFile('./resumes/' + 'juan-anibal-micheli' + '.json', function(err, resume) {
+      res.json([resume])
+  })
+});
+
+app.get('/:name/resume.json', function (req, res) {
+  jsonfile.readFile('./resumes/' + req.params.name + '.json', function(err, resume) {
+    res.json(resume)
+  })
+});
+
+app.post('/:name/resume.json', function (req, res) {
+  jsonfile.writeFile('./resumes/' + req.params.name + '.json', req.params.json, function(err, resume) {
+    res.json(resume)
+  })
+});
 
 app.get('/:name', function (req, res) {
-  // jsonfile.readFile('./resumes/' + req.params.name + '.json', function(err, resume) {
-  //   resumeToHTML(resume, {
-  //     theme: 'abbeal'
-  //   }, function(data) {
-  //     res.send(data)
-  //   })
-  // })
+  jsonfile.readFile('./resumes/' + req.params.name + '.json', function(err, resume) {
+    resumeToHTML(resume, {
+      themeName: 'abbeal-green',
+    }, function(data) {
+      res.send(data)
+    });
+  });
+});
+
+app.get('/:name/edit', function (req, res) {
+  jsonfile.readFile('./resumes/' + req.params.name + '.json', function(err, resume) {
+    resumeToHTML(resume, {
+      themeName: 'abbeal-green',
+      editMode: true
+    }, function(data) {
+      res.send(data)
+    });
+  });
 });
 
 var server = app.listen(3000, function () {
